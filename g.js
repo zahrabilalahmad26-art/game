47c1547012478a8789d1075c1b6c0b8e9d4159e4
zahrabilalahmad26-art <zahrabilalahmad26@gmail.com>
2026-06-06 21:13:58 +0500

    first

---
+++ g.js
@@ -0,0 +1,71 @@
+let paper = document.querySelector(".ic1");
+let rock = document.querySelector(".ic2");
+let scissor = document.querySelector(".ic3");
+let comp;
+let re=document.querySelector(".re");
+let con=document.querySelector(".con");
+function ic1() {
+    let rand = Math.floor(Math.random() * 3);
+
+    if (rand == 0) {
+        comp = "paper";
+        re.innerHTML = "Draw";
+
+        con.innerHTML = `
+            You: <i class="fa-solid fa-hand"></i>
+            Computer: <i class="fa-solid fa-hand"></i>
+        `;
+    }
+
+    else if (rand == 1) {
+        comp = "rock";
+        re.innerHTML = "You Win";
+
+        con.innerHTML = `
+            You: <i class="fa-solid fa-hand"></i>
+            Computer: <i class="fa-solid fa-hand-back-fist"></i>
+        `;
+    }
+
+    else {
+        comp = "scissor";
+        re.innerHTML = "You Lose";
+
+        con.innerHTML = `
+            You: <i class="fa-solid fa-hand"></i>
+            Computer: <i class="fa-solid fa-hand-peace"></i>
+        `;
+    }
+}
+function ic2() {
+    let rand = Math.floor(Math.random() * 3);
+
+    if (rand == 0) {
+        re.innerHTML = "You Lose";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-back-fist"></i> Computer: <i class="fa-solid fa-hand"></i>`;
+    }
+    else if (rand == 1) {
+        re.innerHTML = "Draw";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-back-fist"></i> Computer: <i class="fa-solid fa-hand-back-fist"></i>`;
+    }
+    else {
+        re.innerHTML = "You Win";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-back-fist"></i> Computer: <i class="fa-solid fa-hand-peace"></i>`;
+    }
+}
+function ic3() {
+    let rand = Math.floor(Math.random() * 3);
+
+    if (rand == 0) {
+        re.innerHTML = "You Win";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-peace"></i> Computer: <i class="fa-solid fa-hand"></i>`;
+    }
+    else if (rand == 1) {
+        re.innerHTML = "You Lose";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-peace"></i> Computer: <i class="fa-solid fa-hand-back-fist"></i>`;
+    }
+    else {
+        re.innerHTML = "Draw";
+        con.innerHTML = `You: <i class="fa-solid fa-hand-peace"></i> Computer: <i class="fa-solid fa-hand-peace"></i>`;
+    }
+}
