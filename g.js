fe493817a18cf585b08eee34af6ace79682c8545
zahrabilalahmad26-art <zahrabilalahmad26@gmail.com>
2026-06-06 23:37:26 +0500

    Add game result tracking and update display

--- g.js
+++ g.js
@@ -4,6 +4,7 @@ let scissor = document.querySelector(".ic3");
 let comp;
 let re=document.querySelector(".re");
 let con=document.querySelector(".con");
+let result=document.querySelector(".result");
 function ic1() {
     let rand = Math.floor(Math.random() * 3);
 
@@ -36,6 +37,7 @@ function ic1() {
             Computer: <i class="fa-solid fa-hand-peace"></i>
         `;
     }
+    up();
 }
 function ic2() {
     let rand = Math.floor(Math.random() * 3);
@@ -52,6 +54,7 @@ function ic2() {
         re.innerHTML = "You Win";
         con.innerHTML = `You: <i class="fa-solid fa-hand-back-fist"></i> Computer: <i class="fa-solid fa-hand-peace"></i>`;
     }
+    up();
 }
 function ic3() {
     let rand = Math.floor(Math.random() * 3);
@@ -68,4 +71,25 @@ function ic3() {
         re.innerHTML = "Draw";
         con.innerHTML = `You: <i class="fa-solid fa-hand-peace"></i> Computer: <i class="fa-solid fa-hand-peace"></i>`;
     }
+    up();
+}
+
+let ob={
+    win :0,
+    lose :0,
+    draw :0
+}
+let res=document.querySelector(".re");
+function up() {
+    if (re.innerHTML == "You Win") {
+        ob.win += 1;
+    }
+    else if (re.innerHTML == "You Lose") {
+        ob.lose += 1;
+    }
+    else {
+        ob.draw += 1;
+    }
+
+    result.innerHTML = `Win: ${ob.win} Lose: ${ob.lose} Draw: ${ob.draw}`;
 }
